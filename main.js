99705df3e9eb076ac94ff61c30cf9fd393529954
kellyo64 <francois.flous@gmail.com>
2018-09-26 17:14:23 +0200

    ok

---
+++ main.js
@@ -0,0 +1,12 @@
+function changeColor()
+	{
+		document.body.style.backgroundColor = "#FF00FF"
+	}
+
+
+setTimeout(function(){
+	alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+	window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	}, 10000)
+		
+
